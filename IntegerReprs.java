ee68cf5d4a2e28c9e17444421661872f3080a3a7
Jeanderson Candido <jeandersonbc@gmail.com>
2014-09-27 17:45:20 -0300

    Bits - Playing with bitwise operators

---
+++ IntegerReprs.java
@@ -0,0 +1,43 @@
+import java.lang.StringBuilder;
+
+class Main {
+
+    public static void main(String[] args) {
+        int[] values = {-1 , 1, 2, -2};
+        String binRep;
+        for (int v : values) {
+            binRep = toBinary32bits(v);
+            System.out.printf("(%s)\t", isNegative(v) ? "neg" : "pos");
+            System.out.printf("(mod: %2d)\t", extractModule(v));
+            System.out.printf("%2d -> %32s (%d bits)\n", v, binRep, binRep.length());
+        }
+        System.out.println();
+        for (int a : values) {
+            for (int b : values) {
+                System.out.printf("%2d == %2d ? %s\n", a, b, equals(a, b) ? "same" : "not same");
+            }
+        }
+    }
+    /** returns true if n is a negative value. Shifts the signal bit
+    to the 0-th index and performs a bitwise AND to check if it's negative */
+    private static boolean isNegative(int n) {
+        return ((n >> 31) & 1) > 0;
+    }
+    /** extracts the module of the integer value. One's complement */
+    private static int extractModule(int n) {
+        if (n >= 0) return n;
+
+        int module = ~n + 1;
+        return module;
+    }
+    private static boolean equals(int a, int b) {
+        return (a ^ b) == 0;
+    }
+    private static String toBinary32bits(int n) {
+        StringBuilder str = new StringBuilder();
+        for (int ithBit = 31; ithBit >= 0; ithBit--) {
+            str.append((n >> ithBit) & 1);
+        }
+        return str.toString();
+    }
+}
