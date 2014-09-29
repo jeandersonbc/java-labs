e5485f1398a0dd3be4e18536baf1f044e868f7d1
Jeanderson Candido <jeandersonbc@gmail.com>
2014-09-29 19:53:15 -0300

    Bits - Playing with bitwise operators and primitives

--- src/IntegerReprs.java
+++ src/IntegerReprs.java
@@ -17,15 +17,41 @@ class Main {
                 System.out.printf("%2d == %2d ? %s\n", a, b, equals(a, b) ? "same" : "not same");
             }
         }
-        System.out.println("Comparing >> and >>> operators");
+        System.out.println("\nComparing >> and >>> operators\n");
         int a = 0xABCD1941;
         int b = 0xABCD1941;
         for (int i = 0; i < 32; i++) {
-            b >>>= i;
-            a >>= i;
             System.out.printf("%11d >>  %2d = %s\t", a, i, Integer.toBinaryString(a));
             System.out.printf("%11d >>> %2d = %s\n", b, i, Integer.toBinaryString(b));
+            b >>>= 1;
+            a >>= 1;
         }
+        System.out.println("\nExercises from Thinking in Java 4th\n");
+        // Exercise 11, 12
+        int num = 0x80000000;
+        int num2 = 0xFFFFFFFF;
+        num2 <<= 1;
+        for (int i = 0; i < 32; i++) {
+            System.out.printf("%s\t%s\n", Integer.toBinaryString(num), Integer.toBinaryString(num2));
+            num >>= 1;
+            num2 >>>= 1;
+        }
+        // Exercise 13;
+        char[] chars = {'a', 'j', 'w', 'A', 'J', 'W'};
+        for (char c : chars) {
+            printCharInBinary(c);
+        }
+        for (char c : chars) {
+            char changed = changeCase(c);
+            System.out.printf("%c: %s - %c: %s\n", c, Integer.toBinaryString(c),
+                changed, Integer.toBinaryString(changed));
+        }
+    }
+    private static char changeCase(char c) {
+        return (char)(c ^ (1 << 5));
+    }
+    private static void printCharInBinary(char c) {
+        System.out.printf("%c: %s\n", c, Integer.toBinaryString(c));
     }
     /** returns true if n is a negative value. Shifts the signal bit
     to the 0-th index and performs a bitwise AND to check if it's negative */
